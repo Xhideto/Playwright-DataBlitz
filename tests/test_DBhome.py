336a69d98ec3adfa7ec03f8656cea53bcba23c45
Xhideto <jairellignacio@gmail.com>
2026-06-19 15:41:36 +0800

    Implemented tester for featured consoles products

--- tests/test_DBhome.py
+++ tests/test_DBhome.py
@@ -139,7 +139,7 @@ class TestDatablitzHomepage:
               page.go_back()
               print(f"New arrival product {i+1} been tested")
 
-    def test_new_arrivals_iterate(self, page: Page):
+    def test_new_arrivals_buttons(self, page: Page):
          page.goto("https://ecommerce.datablitz.com.ph/")
 
          product = page.locator("#shopify-section-1590542985479 .product-item").all()
@@ -187,4 +187,39 @@ class TestDatablitzHomepage:
             previous_button = page.locator("#shopify-section-1590542985479").get_by_role("button", name="Previous")
             expect(previous_button).to_be_visible()
             previous_button.click()
+
+    def test_featured_consoles(self, page: Page):
+            page.goto("https://ecommerce.datablitz.com.ph/")
+
+            expect (page.get_by_role("heading", name="Featured Gaming Consoles")).to_be_visible()
+
+            expect (page.locator("#shopify-section-1616576385b0defc44").get_by_text("View all ")).to_be_visible()
+            page.locator('#shopify-section-1616576385b0defc44').get_by_text("View all ").click()
+            expect (page).to_have_url("https://ecommerce.datablitz.com.ph/collections/featured-gaming-consoles")
+            page.go_back()
+
+    def test_featured_consoles_products(self, page: Page):
+            page.goto("https://ecommerce.datablitz.com.ph/")
+
+            # LIMIT=6
+
+            product = page.locator("#shopify-section-1616576385b0defc44 .product-item").count()
+
+            for i in range(product):
+              featuredc_product = page.locator("#shopify-section-1616576385b0defc44 .product-item").nth(i)
+              expect(featuredc_product.locator('.product-item__primary-image')).to_be_visible()
+              featuredc_product.hover()
+              page.wait_for_timeout(500)
+              expect(featuredc_product.locator('.product-item__secondary-image')).to_have_count(1)
+              featuredc_product.click()
+              expect (page).to_have_url(re.compile(r"/products/"))
+              page.go_back()
+
+              expect(featuredc_product.locator('.price')).to_be_visible
+
+              expect(featuredc_product.locator('.product-item__title')).to_be_visible()
+              featuredc_product.locator('.product-item__title').click()
+              expect (page).to_have_url(re.compile(r"/products/"))
+              page.go_back()
+              print(f"Featured consoles product {i+1} been tested")
 
