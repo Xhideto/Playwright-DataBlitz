e15440d8b4b6123f39935d7f9a4719a0930beff0
Xhideto <jairellignacio@gmail.com>
2026-06-25 13:30:15 +0800

    feat: implented test for recommended PS5 games

---
+++ tests/navigation_bar/test_PSfive.py
@@ -0,0 +1,44 @@
+from playwright.sync_api import Page, expect
+import re
+
+class TestNavBarPS5:
+
+    def test_playstation_five_sub_menu(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/")
+
+        psfive = page.locator('.tmenu_navbar')
+
+        psfive.get_by_role('link', name="PS5").hover()
+        page.wait_for_timeout(1000)
+        expect (page.locator('.tmenu_submenu_type_mega')).to_be_visible()
+        print(f"PS5 submenu is seen")
+
+    def test_playstation_five_recommended(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/")
+
+        psfive = page.locator('.tmenu_navbar')
+
+        psfive.get_by_role('link', name="PS5", exact=True).hover()
+        page.wait_for_timeout(500)
+        expect (page.locator('.tmenu_submenu_type_mega')).to_be_visible()
+        print(f"PS5 submenu is works")
+
+        expect (page.get_by_text("PS5 We Recommend")).to_be_visible()
+        print(f"The title is tested")
+
+        recommend_games = page.locator('.tmenu_item_layout_image').count()
+
+        for i in range(recommend_games):
+            page.locator('.tmenu_navbar').get_by_role('link', name="PS5", exact=True).hover()
+            page.wait_for_timeout(500)
+
+            game = page.locator('.tmenu_item_layout_image').nth(i)
+            expect (game.locator('.tmenu_image')).to_be_visible()
+
+            game.hover()
+            page.wait_for_timeout(500)
+            game.locator('.tmenu_item_link').click()
+            expect (page).to_have_url(re.compile(r"/products/"))
+
+            page.go_back()
+            print(f"PS5 recommend game {i+1} tested")
--- tests/navigation_bar/test_home.py
+++ tests/navigation_bar/test_home.py
@@ -6,9 +6,9 @@ class TestNavBarHome:
     def test_nav_bar(self, page: Page):
         page.goto("https://ecommerce.datablitz.com.ph/")
 
-        home = page.locator('.tmenu_navbar')
+        # home = page.locator('.tmenu_navbar')
 
-        expect (home.get_by_role('link', name="Home")).to_be_visible()
-        home.get_by_role('link', name="Home").click()
+        expect (page.locator('.tmenu_navbar').get_by_role('link', name="Home")).to_be_visible()
+        page.locator('.tmenu_navbar').get_by_role('link', name="Home").click()
         expect (page).to_have_url("https://ecommerce.datablitz.com.ph/")
         print(f"it works")
