5638a34394501066b9b201436365c9a3449ae5c3
Xhideto <jairellignacio@gmail.com>
2026-06-29 20:26:06 +0800

    feat: implemented switch games test

--- tests/navigation_bar/test_Switch.py
+++ tests/navigation_bar/test_Switch.py
@@ -97,3 +97,69 @@ class TestNavBarSwitch:
         page.wait_for_timeout(1000)
         page.go_back()
         print(f"'{text}' tested")
+
+    def test_switch_games(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/")
+
+        switch = page.locator('.tmenu_navbar')
+
+        switch.get_by_title('SWITCH').hover()
+        page.wait_for_timeout(500)
+        expect (page.locator('.tmenu_submenu_type_tab')).to_be_visible()
+
+        expect (switch.get_by_role('tab', name="Switch", exact=True)).to_be_visible()
+        switch.get_by_role('tab', name="Switch", exact=True).hover()
+
+        expect (switch.get_by_role('link', name="SWITCH GAMES", exact=True)).to_be_visible()
+
+        switch.hover()
+        page.wait_for_timeout(500)
+        switch.get_by_role('link', name="SWITCH GAMES", exact=True).click()
+        expect (page).to_have_url("https://ecommerce.datablitz.com.ph/collections/nintendo-switch-games")
+
+        page.wait_for_timeout(1000)
+        page.go_back()
+
+        page.locator('.tmenu_navbar').get_by_title('SWITCH', exact=True).hover()
+        page.wait_for_timeout(500)
+
+        games1 = page.locator('.tmenu_item_level_1.tmenu_col-2:has(a[title="SWITCH GAMES"]) .tmenu_item_level_2').count()
+        games2 = page.locator('.tmenu_item_level_1.tmenu_col-2:has(a[title="SWITCH GAMES"]) + .tmenu_item_level_1.tmenu_col-2 .tmenu_item_level_2').count()
+
+        for i in range(games1):
+
+            page.locator('.tmenu_navbar').get_by_title('SWITCH', exact=True).hover()
+            page.wait_for_timeout(500)
+
+            page.get_by_role('tab', name="Switch", exact=True).hover()
+            page.wait_for_timeout(500)
+
+            link = page.locator('.tmenu_item_level_1.tmenu_col-2:has(a[title="SWITCH GAMES"]) .tmenu_item_level_2').nth(i)
+
+            text = link.locator('.tmenu_item_text').inner_text()
+            expect (link.locator('.tmenu_item_text')).to_be_visible()
+
+            link.locator('.tmenu_item_link').evaluate("el => el.click()")
+
+            expect (page).to_have_url(re.compile(r"/collections/"))
+            page.go_back()
+            print(f"Genre '{text}' tested")
+
+        for i in range(games2):
+
+            page.locator('.tmenu_navbar').get_by_title('SWITCH', exact=True).hover()
+            page.wait_for_timeout(500)
+
+            page.get_by_role('tab', name="Switch", exact=True).hover()
+            page.wait_for_timeout(500)
+
+            link = page.locator('.tmenu_item_level_1.tmenu_col-2:has(a[title="SWITCH GAMES"]) + .tmenu_item_level_1.tmenu_col-2 .tmenu_item_level_2').nth(i)
+
+            text = link.locator('.tmenu_item_text').inner_text()
+            expect (link.locator('.tmenu_item_text')).to_be_visible()
+
+            link.locator('.tmenu_item_link').evaluate("el => el.click()")
+
+            expect (page).to_have_url(re.compile(r"/collections/"))
+            page.go_back()
+            print(f"Genre '{text}' tested")
