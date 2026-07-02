4f1eaddcbe6517e0753adc6c09fcbc740cffbe59
Xhideto <jairellignacio@gmail.com>
2026-07-02 16:00:14 +0800

    feat: implemented PC/Mac recommended peripherals & accessories test

--- tests/navigation_bar/test_PCaccessories.py
+++ tests/navigation_bar/test_PCaccessories.py
@@ -28,3 +28,37 @@ class TestNavBarPcAccessories:
         page.wait_for_timeout(1000)
         page.go_back()
         print(f"Peripherals & Accessories sub menu is visible")
+
+    def test_pc_accessories_recommend(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/")
+
+        pc_perip = page.locator('.tmenu_navbar')
+
+        pc_perip.get_by_role('link', name="PC/ Mac").hover()
+        page.wait_for_timeout(500)
+        expect (page.locator('.tmenu_submenu_type_tab')).to_be_visible()
+
+        expect (pc_perip.get_by_role('tab', name="Peripherals & Accessories")).to_be_visible()
+        expect (page.get_by_text("Peripherals & Accessories We Recommend")).to_be_visible()
+        print(f"The text is seen")
+
+        recommend_perip = page.locator('.tmenu_submenu_tab_active .tmenu_item_layout_image').count()
+
+        for i in range(recommend_perip):
+            page.get_by_role('link', name="PC/ Mac").hover()
+            page.wait_for_timeout(500)
+
+            page.get_by_role('tab', name="Peripherals & Accessories").hover()
+            page.wait_for_timeout(500)
+
+            peripherals_access = page.locator('.tmenu_submenu_tab_active .tmenu_item_layout_image').nth(i)
+            expect (peripherals_access.locator('.tmenu_image')).to_be_visible()
+
+            peripherals_access.locator('.tmenu_item_link').click()
+            expect (page).to_have_url(re.compile(r"/products/"))
+
+            page.wait_for_timeout(1000)
+
+            page.go_back()
+            page.wait_for_timeout(500)
+            print(f"PC/Mac recommended peripherals {i+1} tested")
