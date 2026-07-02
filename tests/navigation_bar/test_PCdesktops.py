f1dc6cf6f324126fcda84aa1053676f7cd783f3b
Xhideto <jairellignacio@gmail.com>
2026-07-02 18:52:24 +0800

    feat: implemented PC/Mac categories for laptops & desktops test

--- tests/navigation_bar/test_PCdesktops.py
+++ tests/navigation_bar/test_PCdesktops.py
@@ -42,10 +42,10 @@ class TestNavBarPcDesktops:
             page.get_by_role('tab', name="Laptops & Desktops").hover()
             page.wait_for_timeout(500)
 
-            peripherals_desks = page.locator('.tmenu_submenu_tab_active .tmenu_item_layout_image').nth(i)
-            expect (peripherals_desks.locator('.tmenu_image')).to_be_visible()
+            laps_desks = page.locator('.tmenu_submenu_tab_active .tmenu_item_layout_image').nth(i)
+            expect (laps_desks.locator('.tmenu_image')).to_be_visible()
 
-            peripherals_desks.locator('.tmenu_item_link').click()
+            laps_desks.locator('.tmenu_item_link').click()
 
             url = page.url
 
@@ -61,3 +61,88 @@ class TestNavBarPcDesktops:
             page.go_back()
             page.wait_for_timeout(500)
             print(f"PC/Mac recommended Laptops & Desktops {i+1} tested")
+
+    def test_pc_desktops_categories(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/")
+
+        pc_laps = page.locator('.tmenu_navbar')
+
+        pc_laps.get_by_role('link', name="PC/ Mac").hover()
+        page.wait_for_timeout(500)
+        expect (page.locator('.tmenu_submenu_type_tab')).to_be_visible()
+
+        expect (pc_laps.get_by_role('tab', name="Laptops & Desktops")).to_be_visible()
+        pc_laps.get_by_role('tab', name="Laptops & Desktops").hover()
+
+        expect (pc_laps.locator('.tmenu_submenu_tab_active').get_by_role('link', name="Categories")).to_be_visible()
+        pc_laps.locator('.tmenu_submenu_tab_active').get_by_role('link', name="Categories").click()
+        expect (page).to_have_url("https://ecommerce.datablitz.com.ph/collections/category-nintendo-switch-console?pf_t_categories=Consoles") #When clicked idk why it directs to this link
+
+        page.wait_for_timeout(1000)
+        page.go_back()
+
+        print(f"Categories is working")
+
+        page.locator('.tmenu_navbar').get_by_role('link', name="PC/ Mac").hover()
+        page.wait_for_timeout(500)
+
+        categories1 = page.locator('#tmenu-tabpanel-tmenu-menu-899587 .tmenu_item_level_1.tmenu_col-4:has(a[title="Categories"]) .tmenu_item_level_2').count()
+        categories2 = page.locator('#tmenu-tabpanel-tmenu-menu-899587 .tmenu_item_level_1.tmenu_col-4:has(a[title="Categories"]) + .tmenu_item_level_1.tmenu_col-4 .tmenu_item_level_2').count()
+        categories3 = page.locator('#tmenu-tabpanel-tmenu-menu-899587 .tmenu_item_level_1.tmenu_col-4:has(a[title="Categories"]) + .tmenu_item_level_1.tmenu_col-4 + .tmenu_item_level_1.tmenu_col-4 .tmenu_item_level_2').count()
+
+        for i in range(categories1):
+            page.get_by_role('link', name="PC/ Mac").hover()
+            page.wait_for_timeout(500)
+
+            page.get_by_role('tab', name="Laptops & Desktops").hover()
+            page.wait_for_timeout(500)
+
+            link = page.locator('#tmenu-tabpanel-tmenu-menu-899587 .tmenu_item_level_1.tmenu_col-4:has(a[title="Categories"]) .tmenu_item_level_2').nth(i)
+
+            text = link.locator('.tmenu_item_text').inner_text()
+            expect (link.locator('.tmenu_item_text')).to_be_visible()
+
+            link.locator('.tmenu_item_link').evaluate("el => el.click()")
+
+            expect (page).to_have_url(re.compile(r"/collections/"))
+            page.wait_for_timeout(500)
+            page.go_back()
+            print(f"Categories '{text}' tested")
+
+        for i in range(categories2):
+            page.get_by_role('link', name="PC/ Mac").hover()
+            page.wait_for_timeout(500)
+
+            page.get_by_role('tab', name="Laptops & Desktops").hover()
+            page.wait_for_timeout(500)
+
+            link = page.locator('#tmenu-tabpanel-tmenu-menu-899587 .tmenu_item_level_1.tmenu_col-4:has(a[title="Categories"]) + .tmenu_item_level_1.tmenu_col-4 .tmenu_item_level_2').nth(i)
+
+            text = link.locator('.tmenu_item_text').inner_text()
+            expect (link.locator('.tmenu_item_text')).to_be_visible()
+
+            link.locator('.tmenu_item_link').evaluate("el => el.click()")
+
+            expect (page).to_have_url(re.compile(r"/collections/"))
+            page.wait_for_timeout(500)
+            page.go_back()
+            print(f"Categories '{text}' tested")
+
+        for i in range(categories3):
+            page.get_by_role('link', name="PC/ Mac").hover()
+            page.wait_for_timeout(500)
+
+            page.get_by_role('tab', name="Laptops & Desktops").hover()
+            page.wait_for_timeout(500)
+
+            link = page.locator('#tmenu-tabpanel-tmenu-menu-899587 .tmenu_item_level_1.tmenu_col-4:has(a[title="Categories"]) + .tmenu_item_level_1.tmenu_col-4 + .tmenu_item_level_1.tmenu_col-4 .tmenu_item_level_2').nth(i)
+
+            text = link.locator('.tmenu_item_text').inner_text()
+            expect (link.locator('.tmenu_item_text')).to_be_visible()
+
+            link.locator('.tmenu_item_link').evaluate("el => el.click()")
+
+            expect (page).to_have_url(re.compile(r"/collections/"))
+            page.wait_for_timeout(500)
+            page.go_back()
+            print(f"Categories '{text}' tested")
