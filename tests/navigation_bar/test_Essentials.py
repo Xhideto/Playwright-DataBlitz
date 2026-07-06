74560b24d823cb9cf980f680324dd0612362feeb
Xhideto <jairellignacio@gmail.com>
2026-07-06 12:29:32 +0800

    feat: added recommended products test for more essentials

--- tests/navigation_bar/test_Essentials.py
+++ tests/navigation_bar/test_Essentials.py
@@ -17,3 +17,29 @@ class TestNavBarMoreEssentials:
 
         page.go_back
         print(f"More Essentials page is working")
+
+    def test_more_essentials_recommend(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/")
+
+        page.locator('.tmenu_navbar').get_by_role('link', name="More Essentials", exact=True).hover()
+        page.wait_for_timeout(500)
+        expect (page.locator('.tmenu_submenu_type_mega')).to_be_visible()
+        print(f"More Essentials submenu is visible")
+
+        expect (page.get_by_text("We Recommend")).to_be_visible()
+        print(f"The title is tested")
+
+        recommend_essentials = page.locator('.tmenu_item_layout_image').count()
+
+        for i in range(recommend_essentials):
+            page.locator('.tmenu_navbar').get_by_role('link', name="More Essentials", exact=True).hover()
+
+            essential = page.locator('.tmenu_item_layout_image').nth(i)
+            expect (essential.locator('.tmenu_image')).to_be_visible()
+
+            essential.locator('.tmenu_item_link').click()
+            expect (page).to_have_url(re.compile(r"/products/"))
+
+            page.wait_for_timeout(1000)
+            page.go_back()
+            print(f"Recommended More Essentials {i+1} tested")
