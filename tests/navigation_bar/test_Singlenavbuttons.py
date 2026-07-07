2fc6d8f660959d9d32aa61e69cb91ec47160dda9
Xhideto <jairellignacio@gmail.com>
2026-07-07 17:12:56 +0800

    feat: added test for Promos navigation

--- tests/navigation_bar/test_Singlenavbuttons.py
+++ tests/navigation_bar/test_Singlenavbuttons.py
@@ -17,12 +17,12 @@ class TestNavBarSingleMenus:
     def test_digital_store(self, page: Page):
         page.goto("https://ecommerce.datablitz.com.ph/")
 
-        order = page.locator(".tmenu_navbar")
+        digital = page.locator(".tmenu_navbar")
 
-        order.get_by_role('link', name="Digital Store").hover()
+        digital.get_by_role('link', name="Digital Store").hover()
         page.wait_for_timeout(500)
         with page.expect_popup() as digital_store:
-            order.get_by_role("link", name='Digital Store', exact=True).click()
+            digital.get_by_role("link", name='Digital Store', exact=True).click()
 
         new_page = digital_store.value
         new_page.wait_for_load_state()
@@ -30,3 +30,19 @@ class TestNavBarSingleMenus:
         expect (new_page).to_have_url(re.compile(r"https://digital.datablitz.com.ph/"))
         print(f"'{new_page.url}' is working")
         new_page.close()
+
+    def test_promos(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/")
+
+        promos = page.locator(".tmenu_navbar")
+
+        promos.get_by_role('link', name="Promos").hover()
+        page.wait_for_timeout(500)
+
+        expect (promos.get_by_role('tab', name="HOT DEALS"))
+        print(f"HOT DEALS badge is visible")
+
+        promos.get_by_role("link", name='Promos').click()
+        expect (page).to_have_url("https://ecommerce.datablitz.com.ph/pages/bundles-and-promos")
+        page.wait_for_timeout(1000)
+        print(f"Promos is working")
