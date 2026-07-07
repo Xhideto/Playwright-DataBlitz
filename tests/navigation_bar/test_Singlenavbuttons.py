59b5ba332f2912215051c062d70f43b466fba9a6
Xhideto <jairellignacio@gmail.com>
2026-07-07 17:04:03 +0800

    feat: added test for Digital Store navigation

--- tests/navigation_bar/test_Singlenavbuttons.py
+++ tests/navigation_bar/test_Singlenavbuttons.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import Page, expect
+import re
 
 class TestNavBarSingleMenus:
     def test_pre_orders(self, page: Page):
@@ -12,3 +13,20 @@ class TestNavBarSingleMenus:
         expect (page).to_have_url("https://ecommerce.datablitz.com.ph/pages/pre-orders")
         page.wait_for_timeout(1000)
         print(f"Pre-Orders is working")
+
+    def test_digital_store(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/")
+
+        order = page.locator(".tmenu_navbar")
+
+        order.get_by_role('link', name="Digital Store").hover()
+        page.wait_for_timeout(500)
+        with page.expect_popup() as digital_store:
+            order.get_by_role("link", name='Digital Store', exact=True).click()
+
+        new_page = digital_store.value
+        new_page.wait_for_load_state()
+
+        expect (new_page).to_have_url(re.compile(r"https://digital.datablitz.com.ph/"))
+        print(f"'{new_page.url}' is working")
+        new_page.close()
