0c7fa0f87540a348c3227fc9e1d125337a34f3fa
Xhideto <jairellignacio@gmail.com>
2026-07-13 16:58:58 +0800

    feat: added filters categories options test

--- tests/PS5/filters/test_categories.py
+++ tests/PS5/filters/test_categories.py
@@ -11,7 +11,7 @@ class TestDatablitzFilters:
         page.wait_for_timeout(500)
         print(f"'Filters' title is visible")
 
-    def test_categories_option(self, page: Page):
+    def test_categories(self, page: Page):
         page.goto("https://ecommerce.datablitz.com.ph/collections/playstation-5")
 
         filters = page.locator('.card')
@@ -25,3 +25,22 @@ class TestDatablitzFilters:
         categories.click()
         page.wait_for_timeout(300)
         print(f"Categories option is working")
+
+    def test_categories_options(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/collections/playstation-5")
+
+        filters = page.locator('.card')
+
+        options = filters.locator('.boost-pfs-filter-option-categories .boost-pfs-filter-option-item-list li').count()
+        print(f"{options}")
+
+        for i in range(options):
+            option = filters.locator('.boost-pfs-filter-option-categories .boost-pfs-filter-option-item-list li').nth(i)
+
+            text = option.locator('.boost-pfs-filter-option-value').inner_text()
+            expect (option.locator('.boost-pfs-filter-option-value')).to_be_visible()
+
+            option.locator('.boost-pfs-filter-button').click()
+            expect (page).to_have_url(re.compile(r"collections"))
+            page.go_back()
+            print(f"'{text}' tested")
