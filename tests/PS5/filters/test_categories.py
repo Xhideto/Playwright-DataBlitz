3fcc21597620b6b86afe808433280306d425170f
Xhideto <jairellignacio@gmail.com>
2026-07-13 16:22:32 +0800

    feat: added filters categories visibility and option test

--- tests/PS5/filters/test_categories.py
+++ tests/PS5/filters/test_categories.py
@@ -10,3 +10,18 @@ class TestDatablitzFilters:
         expect (filters.locator('.card__title--small')).to_be_visible()
         page.wait_for_timeout(500)
         print(f"'Filters' title is visible")
+
+    def test_categories_option(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/collections/playstation-5")
+
+        filters = page.locator('.card')
+        categories = filters.get_by_role('button', name="Categories")
+
+        expect (categories).to_be_visible()
+        print(f"'Categories' is visible")
+
+        categories.click()
+        page.wait_for_timeout(300)
+        categories.click()
+        page.wait_for_timeout(300)
+        print(f"Categories option is working")
