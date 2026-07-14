318ca8b33bde90574c51b34cd0f9fb3bf06b738c
Xhideto <jairellignacio@gmail.com>
2026-07-14 08:51:53 +0800

    feat: added filters genre options test

--- tests/PS5/filters/test_genre.py
+++ tests/PS5/filters/test_genre.py
@@ -16,3 +16,23 @@ class TestDatablitzFilters:
         genres.click()
         page.wait_for_timeout(300)
         print(f"Genres option is working")
+
+    def test_genres_options(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/collections/playstation-5")
+
+        filters = page.locator('.card')
+
+        options = filters.locator('.boost-pfs-filter-option-genre .boost-pfs-filter-option-item-list li').count()
+        print(f"{options}")
+
+        for i in range(options):
+            option = filters.locator('.boost-pfs-filter-option-genre .boost-pfs-filter-option-item-list li').nth(i)
+
+            text = option.locator('.boost-pfs-filter-option-value').inner_text()
+            expect (option.locator('.boost-pfs-filter-option-value')).to_be_visible()
+
+            option.locator('.boost-pfs-filter-button').click()
+            expect (page).to_have_url(re.compile(r"collections"))
+            page.locator('.boost-pfs-filter-refine-by-wrapper').locator('button[aria-label="Clear"]').first.click()
+            page.wait_for_timeout(500)
+            print(f"'{text}' tested")
