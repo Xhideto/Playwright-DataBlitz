825ab8dd3d2a691d8bd28e47aea1822badd567e6
Xhideto <jairellignacio@gmail.com>
2026-07-14 15:25:15 +0800

    feat: added filters brand options using checkbox test

--- tests/PS5/filters/test_brand.py
+++ tests/PS5/filters/test_brand.py
@@ -2,17 +2,37 @@ from playwright.sync_api import Page, expect
 import re
 
 class TestDatablitzFilters:
-    def test_brand(self, page: Page):
+    # def test_brand(self, page: Page):
+    #     page.goto("https://ecommerce.datablitz.com.ph/collections/playstation-5")
+
+    #     filters = page.locator('.card')
+    #     brand = filters.get_by_role('button', name="Brand")
+
+    #     expect (brand).to_be_visible()
+    #     print(f"'Brand' is visible")
+
+    #     brand.click()
+    #     page.wait_for_timeout(300)
+    #     brand.click()
+    #     page.wait_for_timeout(300)
+    #     print(f"Brand option is working")
+
+    def test_brand_options_checkbox(self, page: Page):
         page.goto("https://ecommerce.datablitz.com.ph/collections/playstation-5")
 
         filters = page.locator('.card')
-        brand = filters.get_by_role('button', name="Brand")
 
-        expect (brand).to_be_visible()
-        print(f"'Brand' is visible")
+        options = filters.locator('.boost-pfs-filter-option-brand .boost-pfs-filter-option-item-list li').count()
+        print(f"{options}")
 
-        brand.click()
-        page.wait_for_timeout(300)
-        brand.click()
-        page.wait_for_timeout(300)
-        print(f"Brand option is working")
+        for i in range(options):
+            option = filters.locator('.boost-pfs-filter-option-brand .boost-pfs-filter-option-item-list li').nth(i)
+
+            text = option.locator('.boost-pfs-filter-option-value').inner_text()
+            expect (option.locator('.boost-pfs-filter-option-value')).to_be_visible()
+
+            option.locator('.boost-pfs-check-box').click()
+            expect (page).to_have_url(re.compile(r"collections"))
+            page.locator('.boost-pfs-filter-refine-by-wrapper').locator('button[aria-label="Clear"]').first.click()
+            page.wait_for_timeout(500)
+            print(f"'{text}' tested")
