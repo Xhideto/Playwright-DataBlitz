d4a3c34742e39c6cb693bb6326b7cd7783c840f0
Xhideto <jairellignacio@gmail.com>
2026-07-15 09:48:58 +0800

    feat: added filters maximum price input test

--- tests/PS5/filters/test_price.py
+++ tests/PS5/filters/test_price.py
@@ -34,3 +34,21 @@ class TestDatablitzFilters:
         page.locator('.boost-pfs-filter-clear').first.click()
         page.wait_for_timeout(500)
         print(f"Price is now clear")
+
+    def test_max_price_input_box(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/collections/playstation-5")
+
+        filters = page.locator('.card')
+        price_box = filters.locator('.boost-pfs-filter-option-range-amount')
+
+        price_box.get_by_label('Max Price').fill('5000')
+        expect (price_box.get_by_label('Max Price')).to_have_value('5000')
+        page.keyboard.press('Enter')
+
+        expect (page).to_have_url(re.compile(r"/collections/"))
+        page.wait_for_timeout(500)
+        print(f"5000 price range is entered")
+
+        page.locator('.boost-pfs-filter-clear').first.click()
+        page.wait_for_timeout(500)
+        print(f"Price is now clear")
