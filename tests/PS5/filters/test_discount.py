74bd85b5c8e9c44d8726506876ad0da17719dccb
Xhideto <jairellignacio@gmail.com>
2026-07-16 07:54:26 +0800

    feat: added filters discount rate options a button test

--- tests/PS5/filters/test_discount.py
+++ tests/PS5/filters/test_discount.py
@@ -36,3 +36,23 @@ class TestDatablitzFilters:
             page.locator('.boost-pfs-filter-refine-by-wrapper').locator('button[aria-label="Clear"]').first.click()
             page.wait_for_timeout(500)
             print(f"'{text}' tested")
+
+    def test_discount_rate_options_button(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/collections/playstation-5")
+
+        filters = page.locator('.card')
+
+        options = filters.locator('.boost-pfs-filter-option-discount-rate .boost-pfs-filter-option-item-list li').count()
+        print(f"{options}")
+
+        for i in range(options):
+            option = filters.locator('.boost-pfs-filter-option-discount-rate .boost-pfs-filter-option-item-list li').nth(i)
+
+            text = option.locator('.boost-pfs-filter-option-value').inner_text()
+            expect (option.locator('.boost-pfs-filter-option-value')).to_be_visible()
+
+            option.locator('.boost-pfs-filter-button').click()
+            expect (page).to_have_url(re.compile(r"collections"))
+            page.locator('.boost-pfs-filter-refine-by-wrapper').locator('button[aria-label="Clear"]').first.click()
+            page.wait_for_timeout(500)
+            print(f"'{text}' tested")
