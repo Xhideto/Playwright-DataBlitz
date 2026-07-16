c1d1c56f43dedb875935982c998a096d034e019c
Xhideto <jairellignacio@gmail.com>
2026-07-16 08:33:42 +0800

    feat: added filters availability option button test

--- tests/PS5/filters/test_availability.py
+++ tests/PS5/filters/test_availability.py
@@ -32,3 +32,19 @@ class TestDatablitzFilters:
         page.locator('.boost-pfs-filter-refine-by-wrapper').locator('button[aria-label="Clear"]').first.click()
         page.wait_for_timeout(500)
         print(f"'{text}' tested")
+
+    def test_availability_text_button(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/collections/playstation-5")
+
+        filters = page.locator('.card')
+
+        option = filters.locator('.boost-pfs-filter-option-availability .boost-pfs-filter-option-item-list')
+
+        text = option.locator('.boost-pfs-filter-option-value').inner_text()
+        expect (option.locator('.boost-pfs-filter-option-value')).to_be_visible()
+
+        option.locator('.boost-pfs-filter-button').click()
+        expect (page).to_have_url(re.compile(r"collections"))
+        page.locator('.boost-pfs-filter-refine-by-wrapper').locator('button[aria-label="Clear"]').first.click()
+        page.wait_for_timeout(500)
+        print(f"'{text}' tested")
