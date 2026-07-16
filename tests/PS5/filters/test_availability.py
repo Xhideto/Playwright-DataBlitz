b59d362c17eccb2dbe219dbfe08b6c56dcdd1c84
Xhideto <jairellignacio@gmail.com>
2026-07-16 08:31:40 +0800

    feat: added filters availability option using checkbox test

--- tests/PS5/filters/test_availability.py
+++ tests/PS5/filters/test_availability.py
@@ -16,3 +16,19 @@ class TestDatablitzFilters:
         availability.click()
         page.wait_for_timeout(300)
         print(f"Availability option is working")
+
+    def test_availability_checkbox(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/collections/playstation-5")
+
+        filters = page.locator('.card')
+
+        option = filters.locator('.boost-pfs-filter-option-availability .boost-pfs-filter-option-item-list')
+
+        text = option.locator('.boost-pfs-filter-option-value').inner_text()
+        expect (option.locator('.boost-pfs-filter-option-value')).to_be_visible()
+
+        option.locator('.boost-pfs-check-box').click()
+        expect (page).to_have_url(re.compile(r"collections"))
+        page.locator('.boost-pfs-filter-refine-by-wrapper').locator('button[aria-label="Clear"]').first.click()
+        page.wait_for_timeout(500)
+        print(f"'{text}' tested")
