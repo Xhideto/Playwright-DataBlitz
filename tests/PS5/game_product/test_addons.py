2d9baba22654c8e0128e552f4e11114794d7a717
Xhideto <jairellignacio@gmail.com>
2026-07-31 10:31:47 +0800

    feat: added ps5 frequently bought product total price

--- tests/PS5/game_product/test_addons.py
+++ tests/PS5/game_product/test_addons.py
@@ -8,15 +8,28 @@ class TestDatablitzPS5Product:
         frequently_bought = page.locator('.cbb-frequently-bought-container')
 
         title = (frequently_bought.locator('.cbb-frequently-bought-title'))
-        title_text = (title).inner_text()
+        title_text = title.inner_text()
 
         expect (title).to_be_visible()
         print(f"{title_text}")
 
-    def test_product_addon_product_image(self, page: Page):
+    def test_product_addon_image(self, page: Page):
         page.goto("https://ecommerce.datablitz.com.ph/products/ps5-assassins-creed-black-flag-resynced")
 
         frequently_bought = page.locator('.cbb-frequently-bought-container')
 
         expect (frequently_bought.locator('.cbb-frequently-bought-products')).to_be_visible()
         print(f"Frequently brought products tested")
+
+    def test_product_addon_total_price(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/products/ps5-assassins-creed-black-flag-resynced")
+        
+        frequently_bought = page.locator('.cbb-frequently-bought-container')
+
+        expect (frequently_bought.locator('.cbb-frequently-bought-total-price-text')).to_be_visible()
+
+        price = frequently_bought.locator('.cbb-frequently-bought-total-price-regular-price')
+        price_text = price.inner_text()
+
+        expect (price).to_be_visible()
+        print(f"Total price expected: {price_text}")
