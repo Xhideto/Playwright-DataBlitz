04dee58a908792919f9b1ca91600c0f4a0781ef5
Xhideto <jairellignacio@gmail.com>
2026-07-31 10:22:29 +0800

    feat: added ps5 frequently bought products image

--- tests/PS5/game_product/test_addons.py
+++ tests/PS5/game_product/test_addons.py
@@ -12,3 +12,11 @@ class TestDatablitzPS5Product:
 
         expect (title).to_be_visible()
         print(f"{title_text}")
+
+    def test_product_addon_product_image(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/products/ps5-assassins-creed-black-flag-resynced")
+
+        frequently_bought = page.locator('.cbb-frequently-bought-container')
+
+        expect (frequently_bought.locator('.cbb-frequently-bought-products')).to_be_visible()
+        print(f"Frequently brought products tested")
