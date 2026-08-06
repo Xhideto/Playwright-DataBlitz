123d2d534e7196756cde0a15e26c3f0be2957bf5
Xhideto <jairellignacio@gmail.com>
2026-08-06 18:55:44 +0800

    feat: added ps5 frequently bought product checkboxes check and uncheck test

--- tests/PS5/game_product/test_addons.py
+++ tests/PS5/game_product/test_addons.py
@@ -43,3 +43,24 @@ class TestDatablitzPS5Product:
         expect (add_to_cart).to_be_visible()
         expect (add_to_cart).to_be_enabled()
         print(f"Add to card button is working")
+
+    def test_product_addon_checkbox(self, page: Page):
+        page.goto("https://ecommerce.datablitz.com.ph/products/ps5-assassins-creed-black-flag-resynced")
+                
+        frequently_bought = page.locator('.cbb-frequently-bought-container')
+
+        checkboxes = frequently_bought.locator('input[type="checkbox"]').all()
+        assert len(checkboxes) > 0, "No checkboxes found"
+
+        for i, checkbox in enumerate(checkboxes):
+            expect(checkbox).to_be_visible()
+            checked = checkbox.is_checked()
+            print(f"Checkbox {i+1}  checked: {checked}")
+
+            checkbox.uncheck()
+            page.wait_for_timeout(300)
+            assert not checkbox.is_checked(), "Checkbox didn't uncheck"
+            checkbox.check()
+            page.wait_for_timeout(300)
+            assert checkbox.is_checked(), "Checkbox didn't check"
+        print(f"All of the checkboxes work")
